b643604fc95cad5fcad0a696750d5f621148a375
EdwardDunn <edwarddunn12@gmail.com>
2018-05-21 16:14:27 +0100

    implemented millimetre class

--- src/types/Millimetres.java
+++ src/types/Millimetres.java
@@ -1,11 +1,37 @@
+/**
+ * Class - Millimetre
+ * Description - Custom millimetre type that allows for int, double and float values to
+ * be passed in. The returned value is corrected formatted with an optional suffix "mm".
+ * 
+ * Author - Edward Dunn
+ * Version - 1.0
+ */
+
 package types;
 
 public class Millimetres {
 
-	public Millimetres() {
-		
+	private int millimetreValue;
+	private String suffix = "mm";
+	
+	public Millimetres(int _value) {
+		millimetreValue = _value;
 	}
 	
+	public Millimetres(Double _value) {
+		millimetreValue = _value.intValue();
+	}
 	
+	public Millimetres(Float _value) {
+		millimetreValue = _value.intValue();
+	}
+	
+	public int getMillimetre() {
+		return millimetreValue;
+	}
+	
+	public String getMillimetreWithSuffix() {
+		return millimetreValue + suffix;
+	}
 	
 }
