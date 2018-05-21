9aca0b2ddf00fdccd6e5a4fe7e7f5756c197849b
EdwardDunn <edwarddunn12@gmail.com>
2018-05-21 16:20:45 +0100

    implemented Metres class

--- src/types/Metres.java
+++ src/types/Metres.java
@@ -1,9 +1,37 @@
+/**
+ * Class - Metres
+ * Description - Custom metres type that allows for int, double and float values to
+ * be passed in. The returned value is corrected formatted with an optional suffix "m".
+ * 
+ * Author - Edward Dunn
+ * Version - 1.0
+ */
+
 package types;
 
 public class Metres {
 
-	public Metres() {
-		
+	private int metreValue;
+	private String suffix = "m";
+	
+	public Metres(int _value) {
+		metreValue = _value;
+	}
+	
+	public Metres(Double _value) {
+		metreValue = _value.intValue();
+	}
+	
+	public Metres(Float _value) {
+		metreValue = _value.intValue();
+	}
+	
+	public int getMetre() {
+		return metreValue;
+	}
+	
+	public String getmetreWithSuffix() {
+		return metreValue + suffix;
 	}
 	
 }
