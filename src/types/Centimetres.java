404a1ae89cf922bbdc973a2e238d5e515b2e685e
EdwardDunn <edwarddunn12@gmail.com>
2018-05-21 16:23:38 +0100

    Implemented Centimetre class

--- src/types/Centimetres.java
+++ src/types/Centimetres.java
@@ -1,13 +1,38 @@
+/**
+ * Class - Centimetre
+ * Description - Custom centimetre type that allows for int, double and float values to
+ * be passed in. The returned value is corrected formatted with an optional suffix "cm".
+ * 
+ * Author - Edward Dunn
+ * Version - 1.0
+ */
+
 package types;
 
 public class Centimetres {
+
+	private int centimetreValue;
+	private String suffix = "cm";
 	
-	public Centimetres(){
-		
+	public Centimetres(int _value) {
+		centimetreValue = _value;
 	}
 	
+	public Centimetres(Double _value) {
+		centimetreValue = _value.intValue();
+	}
 	
+	public Centimetres(Float _value) {
+		centimetreValue = _value.intValue();
+	}
 	
+	public int getCentimetre() {
+		return centimetreValue;
+	}
 	
-
+	public String getCentimetreWithSuffix() {
+		return centimetreValue + suffix;
+	}
+	
 }
+
